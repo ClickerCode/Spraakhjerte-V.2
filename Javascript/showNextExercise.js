c068470fa48acf50a208cbdf7223e2903187d0ae
Lasse Brustad <lasse.brustad@gmail.com>
2018-11-08 15:06:04 +0100

    Fikset rettingen av linkene på nytt

--- Javascript/showNextExercise.js
+++ Javascript/showNextExercise.js
@@ -5,8 +5,8 @@ function showNextExercise() {
         <p>Er katten er under sofaen?</p>
         <div class="box2"></div>
         <div class="picture2">
-            <a target="_blank" href="../Bilder/oppaa.jpg">
-                <img src="../Bilder/oppaa.jpg" alt="Oppå" width="500" height="300" />
+            <a target="_blank" href="Bilder/oppaa.jpg">
+                <img src="Bilder/oppaa.jpg" alt="Oppå" width="500" height="300" />
             </a>
         </div>
         <button class="button" onclick="showWrong()">Riktig</button>
@@ -35,8 +35,8 @@ function showNextExercise2() {
     exerciseDiv.innerHTML = `
     <div class="box3">Er katten over sofaen?</div>
     <div class="picture3">
-    <a target="_blank" href="../Bilder/over.jpg">
-        <img src="../Bilder/over.jpg" alt="Over" width="500" height="300" />
+    <a target="_blank" href="Bilder/over.jpg">
+        <img src="Bilder/over.jpg" alt="Over" width="500" height="300" />
     </a>
     </div>
     <button class="button" onclick="showCorrect()">Riktig</button>
@@ -50,8 +50,8 @@ function showNextExercise3() {
         <p>Sitter katten på høyre side av sofaen?</p>
         <div class="box2"></div>
         <div class="picture1">
-           <a target="_blank" href="../Bilder/hyre.png">
-            <img src="../Bilder/hyre.png" alt="Høyre" width="500" height="300" />
+           <a target="_blank" href="Bilder/hyre.png">
+            <img src="Bilder/hyre.png" alt="Høyre" width="500" height="300" />
         </a>
         </div>
         <button class="button" onclick="showCorrect()">Riktig</button>
@@ -65,8 +65,8 @@ function showNextExercise4() {
         <p>Er katten under sofaen?</p>
         <div class="box4"></div>
         <div class="picture4">
-            <a target="_blank" href="../Bilder/paa.png">
-                <img src="../Bilder/paa.png" alt="På" width="500" height="300" />
+            <a target="_blank" href="Bilder/paa.png">
+                <img src="Bilder/paa.png" alt="På" width="500" height="300" />
             </a>
         </div>
         <button class="button" onclick="showWrong()">Riktig</button>
@@ -80,8 +80,8 @@ function showNextExercise5() {
         <p>Er katten på venstre side av sofaen?</p>
         <div class="box5"></div>
         <div class="picture5">
-            <a target="_blank" href="../Bilder/over.jpg">
-                <img src="../Bilder/over.jpg" alt="Over" width="500" height="300" />
+            <a target="_blank" href="Bilder/over.jpg">
+                <img src="Bilder/over.jpg" alt="Over" width="500" height="300" />
             </a>
         </div>
         <button class="button" onclick="showWrong()">Riktig</button>
@@ -96,8 +96,8 @@ function showNextExercise6() {
         <p>Sitter katten på venstre siden av sofaen?</p>
         <div class="box6"></div>
         <div class="picture6">
-            <a target="_blank" href="../Bilder/paavenstre.jpg">
-                <img src="../Bilder/paavenstre.jpg" alt="Venstre" width="500" height="300" />
+            <a target="_blank" href="Bilder/paavenstre.jpg">
+                <img src="Bilder/paavenstre.jpg" alt="Venstre" width="500" height="300" />
             </a>
         </div>
         <button class="button" onclick="showCorrect()">Riktig</button>
@@ -111,8 +111,8 @@ function showNextExercise7() {
         <p>Er katten på høyre side av sofaen?</p>
         <div class="box7"></div>
         <div class="picture7">
-            <a target="_blank" href="../Bilder/under.jpg">
-                <img src="../Bilder/under.jpg" alt="Under" width="500" height="300" />
+            <a target="_blank" href="Bilder/under.jpg">
+                <img src="Bilder/under.jpg" alt="Under" width="500" height="300" />
             </a>
         </div>
         <button class="button" onclick="showWrong()">Riktig</button>
@@ -126,8 +126,8 @@ function showNextExercise8() {
         <p>Sitter katten på venstre side av sofaen?</p>
         <div class="box8"></div>
         <div class="picture8">
-            <a target="_blank" href="../Bilder/venstre.png">
-                <img src="../Bilder/venstre.png" alt="Venstre" width="500" height="300" />
+            <a target="_blank" href="Bilder/venstre.png">
+                <img src="Bilder/venstre.png" alt="Venstre" width="500" height="300" />
             </a>
         </div>
         <button class="button" onclick="showCorrect()">Riktig</button>
