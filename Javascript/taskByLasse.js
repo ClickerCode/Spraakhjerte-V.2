9ef7176cc2b8131721b465232f64bea4d533eea8
Lasse Brustad <lasse.brustad@gmail.com>
2018-11-30 13:24:38 +0100

    GitHub Pages kjører slowmo(1e99)

--- Javascript/taskByLasse.js
+++ Javascript/taskByLasse.js
@@ -4,7 +4,11 @@ const medalDiv = document.getElementById('medaljer');
 
 // Last inn bildepakke, og lagre i variablen 'oppgaver'
 function lastOppgaver(mappe) {
-    fetch(`/Spraakhjerte-V.2/Bilder/${mappe}/oppgaver.json`, { cache: 'no-cache' }).then(obj=>obj.json()).then(json=>oppgaver=json);
+    loader(mappe);
+}
+
+function loader(mappe) {
+    fetch(`../Bilder/${mappe}/oppgaver.json`, { cache: 'no-cache' }).then(obj=>obj.json()).then(json=>oppgaver=json);
 }
 
 // Velg oppgave - enten neste eller forrige
